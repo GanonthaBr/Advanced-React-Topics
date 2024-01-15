a84e06a784c2e3afd41d291b8ce1b165685b1dc6
Bruno <Bruno>
2024-01-15 16:21:27 +0100

    adding redux boiler plate

--- src/index.js
+++ src/index.js
@@ -38,6 +38,16 @@ const client = new ApolloClient({
   uri: "https://api.spacex.land/graphql/",
   cache: new InMemoryCache(),
 });
+
+//REDUX
+
+//State
+
+//Action
+
+//Reducer
+
+//Dispatch
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
